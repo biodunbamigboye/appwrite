59dd943ab3f9e03a04ad0fe99db8361822bcfb32
Biodun Bamigboye <36827870+biodunbamigboye@users.noreply.github.com>
2023-09-23 21:22:59 +0000

    WIP

--- index.php
+++ index.php
@@ -1,54 +1,56 @@
 <?php
 
-require('./vendor/autoload.php');
+echo "Hello World!";
 
-use Appwrite\Client;
-use Appwrite\Exception;
-use Appwrite\Services\Databases;
+// require('./vendor/autoload.php');
 
-$html = '<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Contact Form</title>
-  </head>
-  <body>
-    <form action="/" method="POST">
-      <input type="text" id="name" name="name" placeholder="Name" required>
-      <input type="email" id="email" name="email" placeholder="Email" required>
-      <textarea id="content" name="content" placeholder="Message" required></textarea>
-      <button type="submit">Submit</button>
-    </form>
-  </body>
-</html>';
+// use Appwrite\Client;
+// use Appwrite\Exception;
+// use Appwrite\Services\Databases;
 
-return function ($context) {
-  global $html;
+// $html = '<!doctype html>
+// <html lang="en">
+//   <head>
+//     <meta charset="utf-8">
+//     <title>Contact Form</title>
+//   </head>
+//   <body>
+//     <form action="/" method="POST">
+//       <input type="text" id="name" name="name" placeholder="Name" required>
+//       <input type="email" id="email" name="email" placeholder="Email" required>
+//       <textarea id="content" name="content" placeholder="Message" required></textarea>
+//       <button type="submit">Submit</button>
+//     </form>
+//   </body>
+// </html>';
 
-  if ($context->req->method === 'GET') {
-    return $context->res->send($html, 200, ['content-type' => 'text/html']);
-  }
+// return function ($context) {
+//   global $html;
 
-  if ($context->req->method === 'POST' && $context->req->headers['content-type'] === 'application/x-www-form-urlencoded') {
-    \parse_str($context->req->body, $formData);
+//   if ($context->req->method === 'GET') {
+//     return $context->res->send($html, 200, ['content-type' => 'text/html']);
+//   }
+
+//   if ($context->req->method === 'POST' && $context->req->headers['content-type'] === 'application/x-www-form-urlencoded') {
+//     \parse_str($context->req->body, $formData);
     
-    $message = [
-      'name' => $formData['name'],
-      'email' => $formData['email'],
-      'content' => $formData['content']
-    ];
+//     $message = [
+//       'name' => $formData['name'],
+//       'email' => $formData['email'],
+//       'content' => $formData['content']
+//     ];
 
-    $client = new Client();
-    $client
-      ->setEndpoint('https://cloud.appwrite.io/v1')
-      ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
-      ->setKey(getenv('APPWRITE_API_KEY'));
+//     $client = new Client();
+//     $client
+//       ->setEndpoint('https://cloud.appwrite.io/v1')
+//       ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
+//       ->setKey(getenv('APPWRITE_API_KEY'));
 
-    $databases = new Databases($client);
-    $document = $databases->createDocument('[DATABASE_ID]', '[MESSAGES_COLLECTION_ID]', ID::unique(), $message);
+//     $databases = new Databases($client);
+//     $document = $databases->createDocument('[DATABASE_ID]', '[MESSAGES_COLLECTION_ID]', ID::unique(), $message);
 
-    return $context->res->send("Message sent");
-  }
+//     return $context->res->send("Message sent");
+//   }
 
-  return $context->res->send('Not found', 404);
-};
+//   return $context->res->send('Not found', 404);
+// };
