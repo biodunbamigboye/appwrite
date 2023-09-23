ffbe0a02b142e201c89f33ceb94d2967f82856f3
Biodun Bamigboye <36827870+biodunbamigboye@users.noreply.github.com>
2023-09-23 21:57:31 +0000

    WIP

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ return function ($context) {
   global $html;
 
   if ($context->req->method === 'GET') {
-    return $context->res->send($html, 200, ['content-type' => 'text/html']);
+    return $context->res->send($html, 200);
   }
 
   if ($context->req->method === 'POST' && $context->req->headers['content-type'] === 'application/x-www-form-urlencoded') {
