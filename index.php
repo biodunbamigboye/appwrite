bc0a2ba38b3b959985e18b2563d5ed4c9893c0ef
Biodun Bamigboye <36827870+biodunbamigboye@users.noreply.github.com>
2023-09-23 22:01:14 +0000

    WIP

--- index.php
+++ index.php
@@ -8,10 +8,24 @@ use Appwrite\Services\Databases;
 
 
 return function ($context) {
-    $html = '<p>Test Html Data</p>';
+    $html = '<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Contact Form</title>
+      </head>
+      <body>
+        <form action="/" method="POST">
+          <input type="text" id="name" name="name" placeholder="Name" required>
+          <input type="email" id="email" name="email" placeholder="Email" required>
+          <textarea id="content" name="content" placeholder="Message" required></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </body>
+    </html>';
 
   if ($context->req->method === 'GET') {
-    return $context->res->send($html);
+    return $context->res->send($html, 200, ['content-type' => 'text/html']);
   }
 
   if ($context->req->method === 'POST' && $context->req->headers['content-type'] === 'application/x-www-form-urlencoded') {
